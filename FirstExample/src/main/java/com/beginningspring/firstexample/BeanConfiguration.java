f7e5bb3089c0eb1dfaf38d33bdc68302cc102bf3
ObradovicZ <obradtf@gmail.com>
2018-04-29 14:53:31 +0200

    Main class implemented

--- FirstExample/src/main/java/com/beginningspring/firstexample/BeanConfiguration.java
+++ FirstExample/src/main/java/com/beginningspring/firstexample/BeanConfiguration.java
@@ -5,10 +5,24 @@
  */
 package com.beginningspring.firstexample;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
 /**
  *
  * @author javaBrat
  */
+@Configuration
 public class BeanConfiguration {
-    
+    @Bean
+    public AccountService accountService(){
+        AccountServiceImpl bean = new AccountServiceImpl();
+        bean.setAccountDao(accountDao());
+        return bean;
+    }
+    @Bean
+    public AccountDao accountDao(){
+        AccountDaoInMemoryImpl bean = new AccountDaoInMemoryImpl();
+        return bean;
+    }
 }
--- FirstExample/src/main/java/com/beginningspring/firstexample/Main.java
+++ FirstExample/src/main/java/com/beginningspring/firstexample/Main.java
@@ -5,10 +5,30 @@
  */
 package com.beginningspring.firstexample;
 
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
 /**
  *
  * @author javaBrat
  */
 public class Main {
+    public static void main(String[] args) {
+        
+        AnnotationConfigApplicationContext applicationContext =
+new AnnotationConfigApplicationContext(BeanConfiguration.class);
+        
+    AccountService accountService = applicationContext.getBean("accountService",AccountService.class);
     
+    System.out.println("Before money transfer");
+    
+    System.out.println("Account 1 balance :" + accountService.getAccount(1).getBalance());
+    System.out.println("Account 2 balance :" + accountService.getAccount(2).getBalance());
+    
+    accountService.transferMoney(1, 2, 5.0);
+    System.out.println("After money transfer");
+    
+    System.out.println("Account 1 balance :" + accountService.getAccount(1).getBalance());
+    System.out.println("Account 2 balance :" + accountService.getAccount(2).getBalance());
+
+    }
 }
